67aebf38cb0052a5ac0a746eba96d23107adec24
Halimainfo <halimavert333@gmail.com>
2023-03-16 17:40:31 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - is fonction
+ * Return : value equal zero (success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
