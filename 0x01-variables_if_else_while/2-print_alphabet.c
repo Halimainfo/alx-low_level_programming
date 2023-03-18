7c77cc9bf270602d242daccac57092bfc78ffcc4
Halimainfo <halimavert333@gmail.com>
2023-03-17 17:44:55 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - is fonction
+ * Return: 0 value equal 0 (success). 1 otherwise
+ */
+int main(void)
+{
+	int ch = 97;
+
+	for (; ch <= 122; ch++)
+		putchar(ch);
+	return (0);
+}
