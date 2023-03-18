32b91f851ba5a4f987cbb33d4f4ef06accc43db9
Halimainfo <halimavert333@gmail.com>
2023-03-18 16:22:42 -0700

    all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - is fonction
+ * Return: 0 value equal 0 (success). 1 otherwise
+ */
+int main(void)
+{
+	int n;
+	int i;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (n = i + 1; n < 58; n++)
+		{
+			if (i != n)
+			{
+				putchar(i);
+				putchar(n);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		n++;
+	}
+	return (0);
+}
