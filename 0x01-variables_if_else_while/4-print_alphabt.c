1a5c4e3204963ba28a05b7b3ab7c2bbba013b2d8
Halimainfo <halimavert333@gmail.com>
2023-03-17 18:24:45 -0700

    except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - is fonction
+ * Return: 0 value equal 0 (success). 1 otherwise
+ */
+int main(void)
+{
+	int ch = 97;
+
+	for (; ch <= 122; ch++)
+		if (ch == 101 || ch == 113)
+		{
+		ch++;
+		}
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
