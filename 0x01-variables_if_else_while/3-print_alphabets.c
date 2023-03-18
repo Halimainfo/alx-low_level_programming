59db034c5929f5045882906739219cfe495d66d7
Halimainfo <halimavert333@gmail.com>
2023-03-17 18:07:42 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - is fonction
+ * Return: 0 value equal 0 (success). 1 otherwise
+ */
+int main(void)
+{
+	int ch = 97;
+
+	for (; ch <= 122; ch++)
+		putchar(ch);
+	for (ch = 65; ch <= 90; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
