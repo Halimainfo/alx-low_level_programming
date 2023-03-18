62eee7536b3ea91f59bdd3690dc08450ee4be15d
Halimainfo <halimavert333@gmail.com>
2023-03-17 19:05:27 -0700

    possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - is fonction
+ * Return: 0 value equal 0 (success). 1 otherwise
+ */
+int main(void)
+{
+	int c = 48;
+
+	for (; c <= 57; c++)
+	{
+		putchar(c);
+		putchar(' ');
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
