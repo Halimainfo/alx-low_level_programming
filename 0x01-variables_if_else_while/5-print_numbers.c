acba2b86a489b48ff127e41c45ad845c976ee891
Halimainfo <halimavert333@gmail.com>
2023-03-17 18:32:37 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - is fonction
+ * Return: 0 value equal 0 (success). 1 otherwise
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
