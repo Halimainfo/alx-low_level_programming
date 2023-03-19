ae56d08d10b92751cda59bd95f6b3d691ecde69a
Halimainfo <halimavert333@gmail.com>
2023-03-18 17:01:24 -0700

    try 102 not s

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - is fonction
+ * Return: 0 value equal 0 (success). 1 otherwise
+ */
+int main(void)
+{
+	int n;
+	int i;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (n = i + 1; n < 58; n++)
+		{
+				putchar(i);
+				putchar(n);
+				if (i == 56 && n == 57)
+				{
+				continue;
+				}
+				putchar(',');
+				putchar(' ');
+			
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
