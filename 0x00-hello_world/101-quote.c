1e3057b28abca31fbfb818809092ad3160f02498
Halimainfo <halimavert333@gmail.com>
2023-03-20 18:12:13 -0700

    UNIX is simpl

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - is fonction
+ * Return: 0 value equal 0 (success). 1 otherwise
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
