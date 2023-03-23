d4559e0c16a88e669c242299acb510f718c05db6
Halimainfo <halimavert333@gmail.com>
2023-03-22 18:24:30 -0700

    alphabet_lowercasex10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * print_alphabet - is fonction that prints the alphabet, in lowercase
+ */
+void print_alphabet(void)
+{
+	int a = 97;
+	int i = 0;
+
+	while (i < 10)
+	{
+	while (a <= 122)
+	{
+	_putchar(a);
+	a++;
+	}
+	_putchar('\n');
+	i++;
+	}
+}
