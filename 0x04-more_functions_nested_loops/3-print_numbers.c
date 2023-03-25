77b3b428fce379bafe16dbac1d3d65e62cb8943f
Halimainfo <halimavert333@gmail.com>
2023-03-24 17:47:29 -0700

    prints the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * print_numbers - is fonction print numbers
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
