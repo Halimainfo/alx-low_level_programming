58947039b779982df9518b0404d03e3640984b88
Halimainfo <halimavert333@gmail.com>
2023-03-24 18:37:08 -0700

    10 times the numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * more_numbers - is fonction print numbers
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b / 10 != 0)
+			{
+				_putchar(b / 10 + '0');
+			}
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
