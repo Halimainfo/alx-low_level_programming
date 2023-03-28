a2a604f82abf795b2617f3883f2791ba14240aca
Halimainfo <halimavert333@gmail.com>
2023-03-28 16:29:34 -0700

    updates the value it points to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
 /* betty style doc for function main goes there */
 /**
  *reset_to_98 - is fonction updates the value it points to 98
- * @*n: alphabet code ascii to check
+ * @n: alphabet code ascii to check
  * Return: nothing
  */
 void reset_to_98(int *n)
