1bfb43dbcf141c66de8f990505530140aeabdba5
Halimainfo <halimavert333@gmail.com>
2023-03-28 16:28:24 -0700

    updates the value it points to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ *reset_to_98 - is fonction updates the value it points to 98
+ * @*n: alphabet code ascii to check
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
