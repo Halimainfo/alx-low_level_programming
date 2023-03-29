bd92a17bd633c9ca5aa888ad251d76a9d275d979
Halimainfo <halimavert333@gmail.com>
2023-03-28 17:34:51 -0700

    reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * print_rev - is fonction reverse, followed by a new
+ * @s: string
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		l++;
+		i++;
+	}
+	
+	for (i = 0, j = l; i < l; i++)
+	{
+		_putchar(s[j];
+		j--;
+	}
+	_putchar('\n');
+}
