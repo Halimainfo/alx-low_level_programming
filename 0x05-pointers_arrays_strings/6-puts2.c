d1cb879326bed939d4628b617b7947db8f3d4efb
Halimainfo <halimavert333@gmail.com>
2023-03-28 18:33:40 -0700

    other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * puts2 - is fonction reverse, followed by a new
+ * @str: string
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int l = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		l++;
+		i++;
+	}
+	for (i = 0; i < l; i++)
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
