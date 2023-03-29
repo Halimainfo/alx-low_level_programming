c24956de41e8f139b1a8b9021b8a393668bacad2
Halimainfo <halimavert333@gmail.com>
2023-03-28 17:57:15 -0700

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * rev_string - is fonction reverse
+ * @s: string
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int l = 0;
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		l++;
+		i++;
+	}
+	for (i = 0, j = l - 1; i < l; i++)
+	{
+		_putchar(s[j]);
+		j = j - 1;
+	}
+}
