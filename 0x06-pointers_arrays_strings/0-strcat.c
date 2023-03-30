cfe529324eb41c936b56197878b3f1519668e143
Halimainfo <halimavert333@gmail.com>
2023-03-30 14:26:13 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,21 +11,21 @@
 char *_strcat(char *dest, char *src)
 {
 	int l = 0;
-        int i = 0;
+	int i = 0;
 
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
-        i--;
-        dest[i] = ' ';
-        i++;
-        while (src[l] != '\0')
-        {
-                dest[i] = src[l];
-                l++;
-                i++;
-        }
-        dest[i] = '\0';
-        return (dest);
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	dest[i] = ' ';
+	i++;
+	while (src[l] != '\0')
+	{
+		dest[i] = src[l];
+		l++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
