4265cc656d457b2da45512418f09f8d5639e861c
Halimainfo <halimavert333@gmail.com>
2023-03-30 14:01:32 -0700

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * _strcat - is fonction returns the length of a string
+ * @dest: first to con
+ * @src: secned to con
+ * Return: nothing
+ */
+char *_strcat(char *dest, char *src)
+{
+	int l = 0;
+	int i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (src[l] != '\0')
+	{
+		dest[i] = src[l];
+		l++;
+	}
+	return (dest);
+}
