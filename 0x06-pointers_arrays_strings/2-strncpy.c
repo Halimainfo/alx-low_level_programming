97233950da72ef5ccc3ae47815c93697ce12cda9
Halimainfo <halimavert333@gmail.com>
2023-03-30 15:33:42 -0700

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int l = 0;
-	while (src[l] != '\0' && l < n)
-	{
-		dest[l] = src[l];
-		l++;
-	}
-	if (dest[l] != '\0')
-		dest[l] = '\0';
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
 	return (dest);
 }
