dc65a4f48fa4bee645be4d7911d9c86877a9e618
Halimainfo <halimavert333@gmail.com>
2023-03-31 17:59:55 -0700

    lowercase to uppercase.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,13 +10,13 @@
  */
 char *string_toupper(char *c)
 {
-	int i;
+	int i = 0;
 
 	for (i = 0; c[i++]; i++)
 	{
-		if (c[i] < 122 && c[i] > 97)
+		if (c[i] <= 122 && c[i] >= 97)
 		{
-			c[i] = c[i] - 32;
+			c[i] -= 32;
 		}
 	}
 	return (c);
