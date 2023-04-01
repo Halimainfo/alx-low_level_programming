1c656301cdb3b2950ef15eae7ffaaebde02e8806
Halimainfo <halimavert333@gmail.com>
2023-03-31 17:46:14 -0700

    lowercase to uppercase.

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * string_toupper - is fonction changes all lowercase
+ * letters of a string to uppercase
+ * @c: string
+ * Return: char
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c++; index--)
+	{
+		if (c[i] < 122 && c[i] > 97)
+		{
+			c[i] = c[i] - 32;
+		}
+	}
+	return ( *c);
+}
