2d94413be5e21d3bd7bc2bd075ce55d232b912ed
Halimainfo <halimavert333@gmail.com>
2023-04-03 17:45:28 -0700

    copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,6 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
-	/*
-	 * unsigned char *destination = dest;
-	const unsigned char *source = src;
-	*/
 
 	for (index = 0; index < n; index++)
 		dest[index] = src[index];
