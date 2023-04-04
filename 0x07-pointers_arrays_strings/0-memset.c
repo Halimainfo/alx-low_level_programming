065a6cc8b78f0f6f2e171625633e1ca91b7cd7f1
Halimainfo <halimavert333@gmail.com>
2023-04-03 17:30:25 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * _memset - is fonction returns the length of a string
+ * @b: the constant byte
+ * @s: a pointer to the memory area
+ * @n: the first n bytes of the memory area pointed to by s
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
