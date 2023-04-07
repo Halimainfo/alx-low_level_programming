72f613447283b3b16ce6fdaf176842d2ddef1cbb
Halimainfo <halimavert333@gmail.com>
2023-04-06 17:43:32 -0700

    natural square

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - fonction return value of x raised to the power of y.
+ * @x: The number.
+ * @y: the power.
+ * Return: factorial -1 error - the power.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	if ( re * re == n)
+		return (re);
+	re = _sqrt_recursion(n / 2);
+	/*return (re);*/
+}
