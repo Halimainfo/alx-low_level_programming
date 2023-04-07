668bedd2a9b2db91779d946b6e9eaa89a1afe64e
Halimainfo <halimavert333@gmail.com>
2023-04-06 17:28:16 -0700

    pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,21 +3,18 @@
 /**
  * _pow_recursion - fonction return value of x raised to the power of y.
  * @x: The number.
- * @y; the power.
+ * @y: the power.
  * Return: factorial -1 error - the power.
  */
 
 int _pow_recursion(int x, int y)
 {
-	int i = 0;
 	int re = 1;
-	
+
 	if (y < 0)
 		return (-1);
-	
 	if (y == 0)
 		return (1);
-	
 	re *= _pow_recursion(x, y - 1);
 	return (re);
 }
