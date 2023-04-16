08444c5971f0d517513c52ae80487a8e66255925
Halimainfo <halimavert333@gmail.com>
2023-04-15 18:03:30 -0700

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - is fonction.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+        _putchar(argv[0]
+        exit(EXIT_SUCCESS);
+}
