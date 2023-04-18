283810c6f9d8e77a14972292c4f32f02824cf2c0
Halimainfo <halimavert333@gmail.com>
2023-04-17 18:11:52 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef dogh
+#define dogh
+
+/**
+ * struct dog - structur dog.
+ * @name: the dog name.
+ * @age: the dog age.
+ * @owner: the dog owner.
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
+
