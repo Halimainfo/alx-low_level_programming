052636fcabde4cfc7921d4ec4c51b80e2bbaf917
Halimainfo <halimavert333@gmail.com>
2023-04-17 18:33:55 -0700

    initialize struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,14 +1,17 @@
 #include "dog.h"
 
 /**
- * _puts_recursion - fonction (Puts with recursion).
- * @s: The string to print.
+ * init_dog - fonction  initialize dog struct.
+ * @d: The strictur to initialize.
+ * @name : name
+ * @age: age
+ *@owner: owner
  * return: nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = (*name);
-	(*d).age = (*age);
-	(*d).owner = (*owner);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
